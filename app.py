c226093750b5610d1c65c0455c910a76e97a1ed8
Chu1503 <shivakash2003@gmail.com>
2024-06-13 23:18:17 +0530

    fixed cors error

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
-import os
+from flask_cors import CORS 
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/')
 def index():
