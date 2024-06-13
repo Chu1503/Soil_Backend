a434b2ed4bcc2275143067974dd5a06607c0c583
Chu1503 <shivakash2003@gmail.com>
2024-06-14 01:20:29 +0530

    fixed multiple respone error

--- app.py
+++ app.py
@@ -12,23 +12,55 @@ def index():
 
 linear_regression_model = joblib.load('linear_regression_model.pkl')
 gradient_boosting_model = joblib.load('gradient_boosting_regressor_model.pkl')
+neural_network_model = joblib.load('neural_network_model.pkl')
 
-models = {
-    'LinearRegression': linear_regression_model,
-    'GradientBoostingRegressor': gradient_boosting_model,
-}
+@app.route('/predict_linearregressor', methods=['POST', 'OPTIONS'])
+def predict_linear():
+    if request.method == 'OPTIONS':
+        response = app.make_default_options_response()
+    else:
+        data = request.json
+        features = np.array([data['pH'], data['EC'], data['Ava_N'], data['Ava_P'], data['Ava_K']]).reshape(1, -1)
+        prediction = linear_regression_model.predict(features)
+        response = jsonify({'predicted_OC': prediction[0]})
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    data = request.json
-    model_name = data.get('model_name')
-    if model_name not in models:
-        return jsonify({'error': 'Model not found'}), 400
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization')
 
-    model = models[model_name]
-    features = np.array([data['pH'], data['EC'], data['Ava_N'], data['Ava_P'], data['Ava_K']]).reshape(1, -1)
-    prediction = model.predict(features)
-    return jsonify({'predicted_OC': prediction[0]})
+    return response
+
+@app.route('/predict_gradientboostingregressor', methods=['POST', 'OPTIONS'])
+def predict_gradient_boosting():
+    if request.method == 'OPTIONS':
+        response = app.make_default_options_response()
+    else:
+        data = request.json
+        features = np.array([data['pH'], data['EC'], data['Ava_N'], data['Ava_P'], data['Ava_K']]).reshape(1, -1)
+        prediction = gradient_boosting_model.predict(features)
+        response = jsonify({'predicted_OC': prediction[0]})
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization')
+
+    return response
+
+@app.route('/predict_neuralnetwork', methods=['POST', 'OPTIONS'])
+def predict_neural_network():
+    if request.method == 'OPTIONS':
+        response = app.make_default_options_response()
+    else:
+        data = request.json
+        features = np.array([data['pH'], data['EC'], data['Ava_N'], data['Ava_P'], data['Ava_K']]).reshape(1, -1)
+        prediction = neural_network_model.predict(features)
+        response = jsonify({'predicted_OC': prediction[0]})
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Methods', 'POST, OPTIONS')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization')
+
+    return response
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
